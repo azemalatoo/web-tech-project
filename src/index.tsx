80a628f0ad8fb55326e37edf50270211f2b1a3b4
azemkakitaeva <azemkakitaeva@gmail.com>
2024-12-22 23:10:51 +0600

    minor changes added

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+const basename = '/<web-tech-project>';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename={basename}>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
